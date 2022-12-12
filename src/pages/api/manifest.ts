3f3aa38da8fb6d84f5435d438420c2b90e253fe2
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2022-12-12 20:11:50 +0100

    webhook draft

--- src/pages/api/manifest.ts
+++ src/pages/api/manifest.ts
@@ -2,6 +2,7 @@ import { createManifestHandler } from "@saleor/app-sdk/handlers/next";
 import { AppManifest } from "@saleor/app-sdk/types";
 
 import packageJson from "../../../package.json";
+import { invoiceRequestedWebhook } from "./webhooks/invoice-requested";
 
 export default createManifestHandler({
   async manifestFactory(context) {
@@ -9,24 +10,11 @@ export default createManifestHandler({
       name: packageJson.name,
       tokenTargetUrl: `${context.appBaseUrl}/api/register`,
       appUrl: context.appBaseUrl,
-      permissions: [
-        "MANAGE_ORDERS"
-      ],
+      permissions: ["MANAGE_ORDERS"],
       id: "saleor.app",
       version: packageJson.version,
-      webhooks: [
-        /**
-         * Configure webhooks here. They will be created in Saleor during installation
-         * Read more
-         * https://docs.saleor.io/docs/3.x/developer/api-reference/objects/webhook
-         */
-      ],
-      extensions: [
-        /**
-         * Optionally, extend Dashboard with custom UIs
-         * https://docs.saleor.io/docs/3.x/developer/extending/apps/extending-dashboard-with-apps
-         */
-      ],
+      webhooks: [invoiceRequestedWebhook.getWebhookManifest(context.appBaseUrl)],
+      extensions: [],
     };
 
     return manifest;
