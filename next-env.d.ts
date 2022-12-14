d0caefa590b72c7355388167196838c39d0a2761
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2022-12-14 17:54:29 +0100

    fix build

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,9 +3,3 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
-
-declare namespace NodeJS {
-  interface ProcessEnv {
-    TEMP_PDF_STORAGE_DIR: string;
-  }
-}
--- src/modules/invoice-file-name/resolve-temp-pdf-file-location.ts
+++ src/modules/invoice-file-name/resolve-temp-pdf-file-location.ts
@@ -4,5 +4,5 @@ import invariant from "tiny-invariant";
 export const resolveTempPdfFileLocation = (fileName: string) => {
   invariant(fileName.includes(".pdf"), `fileName should include pdf extension`);
 
-  return join(process.env.TEMP_PDF_STORAGE_DIR, fileName);
+  return join(process.env.TEMP_PDF_STORAGE_DIR as string, fileName);
 };
