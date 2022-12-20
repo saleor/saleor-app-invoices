efb2a30fd574c27d95f3bafc27d6ab36aea13610
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2022-12-20 15:00:22 +0100

    Add link to discussions and repo

--- src/pages/configuration.tsx
+++ src/pages/configuration.tsx
@@ -1,16 +1,11 @@
 import { NextPage } from "next";
 import React, { useEffect } from "react";
-import { AlertBase } from "@saleor/macaw-ui";
 import { ChannelsConfiguration } from "../modules/app-configuration/ui/channels-configuration";
 import { trpcClient } from "../modules/trpc/trpc-client";
 import { useRouter } from "next/router";
-import { Typography } from "@material-ui/core";
+import { MainInfo } from "../modules/ui/main-info";
 
 type Tab = "channels";
-
-const alertStyle = {
-  marginBottom: 40,
-};
 
 const ConfigurationPage: NextPage = () => {
   const [activeTab, setActiveTab] = React.useState<Tab>("channels");
@@ -26,13 +21,7 @@ const ConfigurationPage: NextPage = () => {
   return (
     <div>
       <h1>Saleor Invoices</h1>
-      <AlertBase style={alertStyle} variant="info">
-        <Typography paragraph>Generate invoices for Orders in your shop</Typography>
-        <Typography paragraph>
-          Open any order and generate invoice. It will be uploaded and attached to the order. App
-          will use Seller data from configuration below
-        </Typography>
-      </AlertBase>
+      <MainInfo />
 
       {/* Enable if more config available */}
       {/*<PageTabs*/}
