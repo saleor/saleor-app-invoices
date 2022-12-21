15dc4cf768af935e69059f9f91dddb298bc3085a
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2022-12-21 10:45:14 +0100

    pretty print logger only in dev

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -5,12 +5,15 @@ import pino from "pino";
  */
 export const logger = pino({
   level: process.env.APP_DEBUG ?? "silent",
-  transport: {
-    target: "pino-pretty",
-    options: {
-      colorize: true,
-    },
-  },
+  transport:
+    process.env.NODE_ENV === "development"
+      ? {
+          target: "pino-pretty",
+          options: {
+            colorize: true,
+          },
+        }
+      : undefined,
 });
 
 export const createLogger = logger.child.bind(logger);
